b7a51621402835209c58923e82872be8711797f6
Jaerker <johanjakberger@gmail.com>
2024-09-20 13:20:44 +0200

    basic calls done

---
+++ utils/db/agent.js
@@ -0,0 +1,52 @@
+const { DynamoDB } = require('@aws-sdk/client-dynamodb');
+const { DynamoDBDocument } = require('@aws-sdk/lib-dynamodb');
+const { v4: uuid } = require('uuid');
+
+// DB Connection
+const client = new DynamoDB();
+const db = DynamoDBDocument.from(client);
+
+const messages = {
+    getAll: async () => await db.query({
+        TableName: 'shui-message-board-db',
+        KeyConditionExpression: 'pk = :pk',
+        ExpressionAttributeValues: {
+            ':pk': 'message',
+        },
+    }),
+    getById: async (id) => await db.get({
+        TableName: 'shui-message-board-db',
+		Key: {
+            pk:'message',
+            id: id
+        }
+    }),
+    create: async (data) => await db.put({
+        TableName: 'shui-message-board-db',
+        Item: {
+            pk:'message',
+            id: uuid(),
+            createdAt: new Date().toISOString(),
+            updatedAt: new Date().toISOString(),
+            dataBeforeUpdate: {},
+            ...data
+        }
+    }),
+    update: async (id, data) => await db.put({
+        TableName:'shui-message-board-db',
+        Item: data
+    }),
+    deleteById: async (id) => await db.delete({
+        TableName:'shui-message-board-db',
+        Key: {
+            pk:'message',
+            id: id
+        }
+    })
+}
+
+const agent = {
+    messages
+}
+
+module.exports = {agent};
